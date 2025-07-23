ffc3c5b2d01240bbdd220b3e9815e7441b069de2
bySharkster <contact@fernandoaponte.dev>
2025-07-23 15:53:35 -0400

    ShadCN, Prettier, Styles init

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import { ThemeProvider } from '../components/templates/providers/theme-provider';
+
+export const metadata: Metadata = {
+  title: 'Todo App',
+  description: 'Todo App',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=satoshi@400&f[]=bevellier@500&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ apps/web/src/components/atoms/molecules/cards/base.tsx
@@ -0,0 +1,37 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/src/components/atoms/card';
+
+interface CardProps {
+  cardTitle: string;
+  cardDescription: string;
+  cardContent?: React.ReactNode;
+  cardFooter?: React.ReactNode;
+}
+const CardComponent = ({
+  cardTitle,
+  cardDescription,
+  cardContent,
+  cardFooter,
+}: CardProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{cardTitle}</CardTitle>
+        <CardDescription>{cardDescription}</CardDescription>
+      </CardHeader>
+      {cardContent && <CardContent>{cardContent}</CardContent>}
+      <CardFooter>
+        {cardFooter && <CardAction>{cardFooter}</CardAction>}
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default CardComponent;
