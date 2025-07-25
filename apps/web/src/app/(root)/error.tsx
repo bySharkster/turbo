75aa305d7f09df875a421f984ff41c4a95b15db5
bySharkster <contact@fernandoaponte.dev>
2025-07-24 21:58:08 -0400

    feat: implement task management with Supabase integration and UI components

---
+++ apps/web/src/app/(root)/error.tsx
@@ -0,0 +1,43 @@
+'use client'; // Error boundaries must be Client Components
+
+import { useEffect } from 'react';
+import Image from 'next/image';
+import { Button } from '@/src/components/atoms/button';
+import CardComponent from '@/src/components/atoms/molecules/cards/error';
+import Link from 'next/link';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full max-w-2xl h-screen mx-auto">
+          <CardComponent
+            cardTitle="OOPS!"
+            cardDescription="Lost in space."
+            cardContent={ <div className="flex flex-col items-center justify-center gap-2">
+                <Image
+                  src="/assets/error_page.png"
+                  alt="Logo"
+                  width={500}
+                  height={500}
+                />
+              </div>}
+            cardFooter={<div className='flex flex-row items-center justify-center gap-2'>
+                <Button onClick={() => reset()} className="bg-[#6c47ff] text-ceramic-">Try again</Button>
+                <Button asChild variant="secondary">
+                    <Link href="/">Go to home</Link>
+                </Button>
+                </div> }
+          />
+    </div>
+  );
+}
